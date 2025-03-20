a5df0bb24691d4077d25b8b13cd6f87e4db478ae
Thiago Souza Cardoso <thiagocardosok@gmail.com>
2025-03-20 16:22:02 -0300

    funcionalidade de cadastrar cartoes

--- src/main/resources/db/migration/V2__inserir_cliente.sql
+++ src/main/resources/db/migration/V2__inserir_cliente.sql
@@ -1,2 +1,2 @@
 INSERT INTO cliente_entity (cpf, email, nome, role, senha)
-VALUES ('23146991063', 'admin@admin.com', 'Admin', 'ADMIN', '$2a$10$1c0H/W6kb3BKVKIfzm8d1OOm3I1i/keraVOaR93qnnk2FsYByzKH.');
+VALUES ('23146991063', 'admin@bytecard.com', 'Admin', 'ADMIN', '$2a$10$1c0H/W6kb3BKVKIfzm8d1OOm3I1i/keraVOaR93qnnk2FsYByzKH.');
